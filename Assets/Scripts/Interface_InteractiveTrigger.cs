94f7bc38fed32bb7472a60ad49ba5a0ab0ce6c82
CodeFluent <wasfimomenwim@gmail.com>
2016-01-21 23:53:58 -0500

    Messy Unity Build so Far
    This is the work so far in the 3D Project, clean build to come later

---
+++ Assets/Scripts/Interface_InteractiveTrigger.cs
@@ -0,0 +1,52 @@
+﻿//----------------------------------------------------------------------------------------------------
+//    3D Atlanta Project : Student Innovation Fellowship
+//		Georgia State University
+//
+//		Interface Interaction Trigger: This script is attached to any interactive object. If the 
+//			player controls script sends an activation 'message' to this script attached
+//			attached to the object, then it will highlight the object. The process of this script is that
+//			as long as the player controls are sending a message, it will indefinitely add .05 seconds to
+//			a timer in this script "timeToNextCheckForCollision"  -- When no message is recieved, this script
+//			will turn off the renderer for the "highlight box" that surrounds the object in .05 seconds.
+//
+//
+//----------------------------------------------------------------------------------------------------
+
+
+
+
+using UnityEngine;
+using System.Collections;
+
+public class Interface_InteractiveTrigger : MonoBehaviour {
+
+	public bool mouseIsOverTrigger = false;
+	public float timeToNextCheckForCollision;
+
+	//this variable is defined in the game engine's sidebar. Each object type should have a unique identifier, 
+	//	ie. Did you Knows are assigned to value 1, booklets '0'
+	public int objectCode = 1;
+
+	// Use this for initialization
+	void Start () {
+	//nothing to initialize yet--maybe in the future so it's simply left blank
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	//if the player controls mouse cursor or proximity sensor is sending a message that it's 
+	//	touching the trigger, turn the renderer of the trigger on (or highlight it a specific color)
+	if (timeToNextCheckForCollision > Time.time)
+		GetComponent<Renderer>().enabled = true;
+	//else turn the renderer off
+	else 
+		GetComponent<Renderer>().enabled = false;
+	}
+
+
+	public void ActivateHighlightTrigger(){
+		timeToNextCheckForCollision = Time.time + .05f;
+	}
+
+}
---
+++ Assets/Scripts/Vehicle_Movement.cs
@@ -0,0 +1,65 @@
+﻿//----------------------------------------------------------------------------------------------------
+//    3D Atlanta Project : Student Innovation Fellowship
+//		Georgia State University
+//
+//		Player Controls: This script handles all player controls that affect interaction, movement, 
+//			and other player-based activities
+//
+//
+//----------------------------------------------------------------------------------------------------
+
+
+
+
+using UnityEngine;
+using System.Collections;
+
+public class Vehicle_Movement : MonoBehaviour {
+
+	public Vector3 targetDirection = Vector3.zero;
+	public Vector3 destinationNode = Vector3.zero;
+
+	public GameObject vehicleTrackNodes;
+	public int currentNodeIndex;
+
+	public float vehicleSpeed = 15f;
+
+	// Use this for initialization
+	void Start () {
+		destinationNode = vehicleTrackNodes.transform.GetChild (0).transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Update Movement Every Update and Move the Vehicle towards the current node point
+		//targetDirection = transform.TransformDirection (destinationNode - transform.position);
+		//Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDirection, Time.deltaTime * 2, 0.0F);
+		//Debug.DrawRay(transform.position, newDir, Color.red);
+		//transform.rotation = Quaternion.LookRotation(newDir);
+		transform.LookAt (destinationNode);
+		gameObject.GetComponent<CharacterController>().Move(transform.forward * vehicleSpeed * Time.deltaTime);
+
+
+
+
+
+	}
+
+	public void goToNextTrackNode(){
+		if (currentNodeIndex < vehicleTrackNodes.transform.childCount - 1)
+			currentNodeIndex++;
+
+		else
+			currentNodeIndex = 0;
+
+		destinationNode = vehicleTrackNodes.transform.GetChild (currentNodeIndex).transform.position;
+					
+	}
+
+	public void OnTriggerEnter(Collider node){
+		if (node.tag == "railTrackNode")
+			goToNextTrackNode ();
+
+	}
+}

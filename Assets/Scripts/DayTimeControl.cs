a30898d94aa1021ceaebdf7516e32d6cbc4c68d9
Jack Le <Jack Le>
2016-03-11 04:20:12 -0500

    got the sun running and the skybox rolling

--- Assets/Scripts/DayTimeControl.cs
+++ Assets/Scripts/DayTimeControl.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 [RequireComponent(typeof(Light))]
 public class DayTimeControl : MonoBehaviour {
-    public Light[] streetLight;
     public float startTime;
     public float timeFlowingRate;
     public float sunRiseTime;
     public float sunSetTime;
+    public float streetLightOnTime;
+    public float streetLightOffTime;
     public float maxIntensity;
-    public float streetLightIntensity;
 
     private Light sunLight;
     public float currentTime;
@@ -24,21 +24,36 @@ public class DayTimeControl : MonoBehaviour {
         currentTime += timeFlowingRate * Time.deltaTime;
         currentTime %= 24;
 
-        float sunAngle = (currentTime - sunRiseTime) * 360 / 24;
-        float sunIntensity = Mathf.Clamp(Mathf.Sin(sunAngle * Mathf.PI / 360), 0, 1) * maxIntensity;
+        float alpha;
+        float sunAngle = 0;
 
+        if (currentTime < 12) {
+            alpha = (currentTime - sunRiseTime) / (12 - sunRiseTime);
+            sunAngle = linear(0, 90, alpha);
+        } else {
+            alpha = (currentTime - 12) / (sunSetTime - 12);
+            sunAngle = linear(90, 180, alpha);
+        }
+
+        float sunIntensity = linear(0, maxIntensity, Mathf.Sin(sunAngle * Mathf.PI / 180)); 
         sunLight.intensity = sunIntensity;
         sunLight.transform.localEulerAngles = new Vector3(sunAngle, 90, 0);
-        if (currentTime >= sunSetTime || currentTime <= sunRiseTime) {
-            turnStreetLight(streetLightIntensity);
+        if (currentTime >= streetLightOnTime || currentTime <= streetLightOffTime) {
+            turnStreetLight(true);
         } else {
-            turnStreetLight(0);
+            turnStreetLight(false);
         }
     }
 
-    void turnStreetLight(float intensity) {
-        for (int i = 0; i < streetLight.Length; i++) {
-            streetLight[i].intensity = intensity;
+    void turnStreetLight(bool turn) {
+        GameObject[] streetLight = GameObject.FindGameObjectsWithTag("Street Light");
+        foreach (GameObject light in streetLight) {
+            ((Light) light.GetComponent(typeof(Light))).enabled = turn;
         }
+    }
+
+    // Linear function. The return value is a when alpha = 0, b when alpha = 1, between a and be when 0 < alpha < 1.
+    float linear(float a, float b, float alpha) {
+        return a * (1 - alpha) + b * alpha;
     }
 }

803a62ead69505c103cc85e762030b5ffcd39bcd
jackle1127 <nanotek2711@yahoo.com>
2016-04-18 10:31:07 -0400

    commit to pull

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -19,47 +19,50 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        // Movement input
-        if (jumpable) { // Player also needs to be grounded to move
-            float verticalMovement = Input.GetAxisRaw("Vertical");
-            Vector3 forwardVector = transform.forward * verticalMovement;
-            float horizontalMovement = Input.GetAxisRaw("Horizontal");
-            Vector3 rightVector = transform.right * horizontalMovement;
+        if (Time.timeScale > 0) {
+            // Movement input
+            if (jumpable) { // Player also needs to be grounded to move
+                float verticalMovement = Input.GetAxisRaw("Vertical");
+                Vector3 forwardVector = transform.forward * verticalMovement;
+                float horizontalMovement = Input.GetAxisRaw("Horizontal");
+                Vector3 rightVector = transform.right * horizontalMovement;
 
-            Vector3 movementVector = forwardVector + rightVector;
-            movementVector.Normalize();
-            if (movementVector.magnitude > 0) {
-                camera.GetComponent<Animator>().enabled = true;
-                rigidbody.velocity += movementVector * acceleration / Time.deltaTime;
-                float currentVelocity = Mathf.Sqrt(
-                    rigidbody.velocity.x * rigidbody.velocity.x
-                    + rigidbody.velocity.z * rigidbody.velocity.z);
-                float maxSpeed = (Input.GetAxisRaw("Run") > 0) ? runningSpeed : walkingSpeed;
-                if (currentVelocity > maxSpeed) {
-                    Vector3 currentVector = rigidbody.velocity;
-                    currentVector.x = currentVector.x * maxSpeed / currentVelocity;
-                    currentVector.z = currentVector.z * maxSpeed / currentVelocity;
-                    rigidbody.velocity = currentVector;
+                Vector3 movementVector = forwardVector + rightVector;
+                movementVector.Normalize();
+                if (movementVector.magnitude > 0) {
+                    camera.GetComponent<Animator>().enabled = true;
+                    rigidbody.velocity += movementVector * acceleration / Time.deltaTime;
+                    float currentVelocity = Mathf.Sqrt(
+                        rigidbody.velocity.x * rigidbody.velocity.x
+                        + rigidbody.velocity.z * rigidbody.velocity.z);
+                    float maxSpeed = (Input.GetAxisRaw("Run") > 0) ? runningSpeed : walkingSpeed;
+                    if (currentVelocity > maxSpeed) {
+                        Vector3 currentVector = rigidbody.velocity;
+                        currentVector.x = currentVector.x * maxSpeed / currentVelocity;
+                        currentVector.z = currentVector.z * maxSpeed / currentVelocity;
+                        rigidbody.velocity = currentVector;
+                    }
                 }
-            } else {
-                camera.GetComponent<Animator>().enabled = false;
-                camera.GetComponent<Animator>().SetTime(0);
+                else {
+                    camera.GetComponent<Animator>().enabled = false;
+                    camera.GetComponent<Animator>().SetTime(0);
+                }
             }
-        }
 
-        if (Input.GetAxisRaw("Jump") > 0 && jumpable) {
-            rigidbody.AddForce(transform.up * jumpStrenth);
-            jumpable = false;
-            groundTime = 0;
-        }
-
-
-
-        if (Mathf.Abs(rigidbody.velocity.y) < 0.01) {
-            groundTime += Time.deltaTime;
-            if (groundTime >= .1) {
-                jumpable = true;
+            if (Input.GetAxisRaw("Jump") > 0 && jumpable) {
+                rigidbody.AddForce(transform.up * jumpStrenth);
+                jumpable = false;
+                groundTime = 0;
             }
+
+            if (Mathf.Abs(rigidbody.velocity.y) < 0.01) {
+                groundTime += Time.deltaTime;
+                if (groundTime >= .1) {
+                    jumpable = true;
+                }
+            }
+        } else {
+            camera.GetComponent<Animator>().enabled = false;
         }
     }
 

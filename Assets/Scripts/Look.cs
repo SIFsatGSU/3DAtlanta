8600c45db9f2594c4419633d63aea45d29df4a3e
Jack <nanotek2711@yahoo.com>
2016-09-14 16:35:29 -0400

    minor updates

--- Assets/Scripts/Look.cs
+++ Assets/Scripts/Look.cs
@@ -16,11 +16,11 @@ public class Look : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         if (Time.timeScale > 0) {
-            float mouseX = Input.GetAxisRaw("Mouse X") + Input.GetAxisRaw("Look X");
-            float mouseY = Input.GetAxisRaw("Mouse Y") + Input.GetAxisRaw("Look Y");
+            float deltaX = Input.GetAxisRaw("Mouse X") + Input.GetAxisRaw("Look X");
+            float deltaY = Input.GetAxisRaw("Mouse Y") + Input.GetAxisRaw("Look Y");
 
-            transform.Rotate(0, sensitivityX * mouseX, 0);
-            rotationY += mouseY * sensitivityY;
+			transform.Rotate(0, sensitivityX * deltaX, 0);
+			rotationY += deltaY * sensitivityY;
             rotationY = Mathf.Clamp(rotationY, -limitY / 2, limitY / 2);
             camera.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
         }
